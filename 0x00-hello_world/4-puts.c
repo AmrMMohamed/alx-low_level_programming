226dc55d47d64d0f32a05127bc009283fdd02b2e
AmrMMohamed <amrmohamedd2222@gmail.com>
2023-03-16 09:58:54 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/*
+ * main - Entry point 
+ * Descrioton a c program that prints with put function
+ * return 0 for (success)
+ * */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
