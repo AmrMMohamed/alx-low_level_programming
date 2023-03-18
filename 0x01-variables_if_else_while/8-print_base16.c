48316518ee3168d4a0a9fc6056df78a122928aa9
AmrMMohamed <amrmohamedd2222@gmail.com>
2023-03-18 12:09:50 -0700

     numbers of base 16 in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  numbers of base 16 in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
