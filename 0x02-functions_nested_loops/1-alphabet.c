a5df4e2315c599e7747d8d91dd503cc90da0d18e
AmrMMohamed <amrmohamedd2222@gmail.com>
2023-03-21 09:18:27 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ *
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+	for (char i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
