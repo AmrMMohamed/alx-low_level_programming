f93c6d8d0f8fc4e1477f6c6b2973e28815f5196f
AmrMMohamed <amrmohamedd2222@gmail.com>
2023-04-06 14:24:27 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_divisible - checks if n is divisible by any integer between 2 and sqrt(n)
+ *
+ * @n: the number to check for divisibility
+ * @divisor: the current divisor being checked
+ *
+ * Return: 1 if n is divisible by divisor, 0 otherwise
+ */
+int is_divisible(int n, int divisor)
+{
+	if (divisor == 1)
+		return (0);
+	if (divisor * divisor > n)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_divisible(n, divisor + 1));
+}
+
+/**
+ * is_prime_number - checks if a number is prime
+ *
+ * @n: the number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (is_divisible(n, 2));
+}
