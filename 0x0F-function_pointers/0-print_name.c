2de12792fd08e75c976be3c7d45951f2d7e92f23
AmrMMohamed <amrmohamedd2222@gmail.com>
2023-04-21 15:59:47 -0700

    0-print

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "FUNCTION_POINTERS.h"
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name
@@ -10,5 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name && f)
+		f(name);
 }
